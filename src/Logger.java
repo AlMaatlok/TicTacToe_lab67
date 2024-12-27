2c3c7b893ef8a4703ea7e19dd1ac0865fa981ee4
Alicja Matłok <163751160+AlMaatlok@users.noreply.github.com>
2024-12-27 20:34:55 +0100

    klasa RoomServer

--- src/Logger.java
+++ src/Logger.java
@@ -1,2 +1,8 @@
 public class Logger {
+    public Logger() {
+    }
+    public void logInfo(String message){
+
+    }
 }
+
--- src/RoomServer.java
+++ src/RoomServer.java
@@ -1,3 +1,29 @@
-public class RoomServer {
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
+public class RoomServer {
+    private SessionManager sessionManager;
+    private Logger logger;
+
+    public RoomServer(SessionManager sessionManager, Logger logger) {
+        this.sessionManager = sessionManager;
+        this.logger = logger;      //do poprawy
+    }
+
+    public void startSever(){
+        try{
+            RoomServiceImpl roomService = new RoomServiceImpl(sessionManager);
+            RoomServiceImpl stub = (RoomServiceImpl) UnicastRemoteObject.exportObject(roomService, 0);
+
+            Registry registry = LocateRegistry.createRegistry(1099);
+
+            registry.rebind("RoomService", stub);
+
+        }
+        catch(Exception e){
+        }
+    }
+
+    public void logInfo(String info){}
 }
